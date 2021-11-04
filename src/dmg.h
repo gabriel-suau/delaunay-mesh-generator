0e59d180b70a7d3449aaa522543a439d7af0ef9c
Gabriel Suau <gabrielsuau2@gmail.com>
2021-11-04 21:29:40 +0100

    Add function to compute the circumcircle of a triangle

--- src/dmg.h
+++ src/dmg.h
@@ -16,13 +16,16 @@ int DMG_saveQual_medit(DMG_pMesh mesh, char *filename);
 /* Quality */
 int DMG_computeQual(DMG_pMesh mesh);
 double DMG_computeArea(double a[2], double b[2], double c[2]);
-double DMG_computeTriaArea(DMG_pMesh mesh, DMG_Tria *pt);
-double DMG_computeTriaQual(DMG_pMesh mesh, DMG_Tria *pt);
+double DMG_computeTriaArea(DMG_pMesh mesh, DMG_pTria pt);
+double DMG_computeTriaQual(DMG_pMesh mesh, DMG_pTria pt);
 int DMG_displayQualHisto(DMG_pMesh mesh, int nclass);
 
 /* Locate */
-int DMG_baryCoord(DMG_pMesh mesh, DMG_Tria *pt, double c[2], double *det, double bc[3]);
+int DMG_baryCoord(DMG_pMesh mesh, DMG_pTria pt, double c[2], double *det, double bc[3]);
 int DMG_locTria(DMG_pMesh mesh, int start, double c[2], double bc[3]);
+int DMG_findCircumcircle(DMG_pMesh mesh, DMG_pTria pt, double c[2], double *rad);
+int DMG_isInCircumcircle(DMG_pMesh mesh, double c[2], DMG_pTria pt);
+int DMG_createCavity(DMG_pMesh mesh, double c[2], int it, int *list);
 
 /* Adjacency relations */
 int DMG_hashHedge(DMG_pMesh mesh);
--- src/locate_dmg.c
+++ src/locate_dmg.c
@@ -60,3 +60,60 @@ int DMG_locTria(DMG_pMesh mesh, int start, double c[2], double bc[3]) {
 
   return it;
 }
+
+/* rad is the radius squared */
+int DMG_findCircumcircle(DMG_pMesh mesh, DMG_pTria pt, double c[2], double *rad) {
+  double *c1, *c2, *c3, m1[2], m2[2], sl1, sl2, b1, b2;
+
+  c1 = mesh->point[pt->v[0]].c;
+  c2 = mesh->point[pt->v[0]].c;
+  c3 = mesh->point[pt->v[0]].c;
+
+  /* Middle of local edge n°1 */
+  m1[0] = 0.5 * (c3[0] - c2[0]);
+  m1[1] = 0.5 * (c3[1] - c2[1]);
+  /* Middle of local edge n°2 */
+  m2[0] = 0.5 * (c3[0] - c1[0]);
+  m2[1] = 0.5 * (c3[1] - c1[1]);
+
+  /* Inverse opposite of the slopes of the segments */
+  sl1 = (c2[0] - c3[0]) / (c3[1] - c2[1]);
+  sl2 = (c1[0] - c3[0]) / (c3[1] - c1[1]);
+
+  /* Ordonee a lorigine */
+  b1 = m1[1] - sl1 * m1[0];
+  b2 = m2[1] - sl2 * m2[0];
+
+  /* Circumcenter coordinates */
+  c[0] = (b2 - b1) / (sl1 - sl2);
+  c[1] = sl2 * c[0] + b2;
+
+  /* Squared radius */
+  *rad = (c1[0] - c[0]) * (c1[0] - c[0]) + (c1[1] - c[1]) * (c1[1] - c[1]);
+
+  return DMG_SUCCESS;
+}
+
+
+int DMG_isInCircumcircle(DMG_pMesh mesh, double c[2], DMG_pTria pt) {
+  double center[2], rad, dist;
+
+  DMG_findCircumcircle(mesh, pt, center, &rad);
+
+  dist = (c[0] - center[0]) * (c[0] - center[0]) + (c[1] - center[1]) * (c[1] - center[1]);
+
+  return (dist < rad ? 1 : 0);
+}
+
+
+int DMG_createCavity(DMG_pMesh mesh, double c[2], int it, int *list) {
+  DMG_pTria pt;
+  int i;
+
+  list  = (int*)malloc(sizeof(int));
+  list[0] = it;
+
+  pt = &mesh->tria[it];
+  
+  return DMG_SUCCESS;
+}
