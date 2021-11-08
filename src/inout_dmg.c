3c389d26fbf5ec34a877e0387ff60151f29e769f
Gabriel Suau <gabrielsuau2@gmail.com>
2021-11-08 20:14:13 +0100

    Change the way we read the mesh

--- src/inout_dmg.c
+++ src/inout_dmg.c
@@ -6,6 +6,7 @@ int DMG_loadMesh_medit(DMG_pMesh mesh, char *filename) {
   DMG_pPoint ppt;
   DMG_pEdge pa;
   DMG_pTria pt;
+  int posnp, posna, posnt;
   char chain[127];
   int i, tmp;
   double dummy;
@@ -21,78 +22,97 @@ int DMG_loadMesh_medit(DMG_pMesh mesh, char *filename) {
     return DMG_FAILURE;    
   }
 
+  /* Scan a first time to get the nb of entities */
   while(fscanf(file, "%s", chain) != EOF && strncmp(chain, "End", strlen("End"))){
 
     if (!strncmp(chain, "MeshVersionFormatted", strlen("MeshVersionFormatted"))) {
       fscanf(file, "%d", &mesh->ver);
       continue;
     }
-
-    if (!strncmp(chain, "Dimension", strlen("Dimension"))) {
+    else if (!strncmp(chain, "Dimension", strlen("Dimension"))) {
       fscanf(file, "%d", &mesh->dim);
       continue;
     }
-
-    if (!strncmp(chain, "Vertices", strlen("Vertices"))) {
+    else if (!strncmp(chain, "Vertices", strlen("Vertices"))) {
       fscanf(file, "%d", &mesh->np);
-      mesh->point = (DMG_Point*) malloc(mesh->np * sizeof(DMG_Point));
-      if (mesh->dim == 2) {
-        for (i = 0 ; i < mesh->np ; i++) {
-          ppt = &mesh->point[i];
-          fscanf(file, "%lf %lf %d", &ppt->c[0], &ppt->c[1], &ppt->ref);
-        }
-      } else if (mesh->dim == 3) {
-        mesh->dim = 2;
-        for (i = 0 ; i < mesh->np ; i++) {
-          ppt = &mesh->point[i];
-          fscanf(file, "%lf %lf %lf %d", &ppt->c[0], &ppt->c[1], &dummy, &ppt->ref); 
-        }
-      }
+      posnp = ftell(file);
       continue;
     }
-
-    if (!strncmp(chain, "Edges", strlen("Edges"))) {
+    else if (!strncmp(chain, "Edges", strlen("Edges"))) {
       fscanf(file, "%d", &mesh->na);
-      mesh->edge = (DMG_Edge*) malloc(mesh->na * sizeof(DMG_Edge));
-      for (i = 0 ; i < mesh->na ; i++) {
-        pa = &mesh->edge[i];
-        fscanf(file, "%d %d %d", &pa->v[0], &pa->v[1], &pa->ref);
-        pa->v[0]--;
-        pa->v[1]--;
-      }
+      posna = ftell(file);
       continue;
     }
-
-    if (!strncmp(chain, "Triangles", strlen("Triangles"))) {
+    else if (!strncmp(chain, "Triangles", strlen("Triangles"))) {
       fscanf(file, "%d", &mesh->nt);
-      mesh->tria = (DMG_Tria*) malloc(mesh->nt * sizeof(DMG_Tria));
-
-      for (i = 0 ; i < mesh->nt ; i++) {
-        pt = &mesh->tria[i];
-        fscanf(file, "%d %d %d %d", &pt->v[0], &pt->v[1], &pt->v[2], &pt->ref);
-        pt->v[0]--;
-        pt->v[1]--;
-        pt->v[2]--;
-
-        pt->flag = 0;
-
-        if (DMG_computeTriaArea(mesh, pt) < 0.) {
-          tmp = pt->v[2];
-          pt->v[2] = pt->v[1];
-          pt->v[1] = tmp;
-        }
-      }
-
+      posnt = ftell(file);
       continue;
     }
 
   }
 
+  if (!mesh->np) {
+    fprintf(stdout, "Error: %s:%d : No point in the input mesh\n", __func__, __LINE__);
+    return DMG_FAILURE;
+  }
+
+  /* Allocate memory */
+  if (DMG_allocMesh(mesh) == DMG_FAILURE) {
+    fprintf(stderr, "Error! %s:%d : Could not allocate the mesh entities arrays.\n", __func__, __LINE__);
+    return DMG_FAILURE;
+  }
+
+  /* Read the mesh */
+
+  /* Vertices */
+  rewind(file);
+  fseek(file, posnp, SEEK_SET);
+  if (mesh->dim == 2) {
+    for (i = 0 ; i < mesh->np ; i++) {
+      ppt = &mesh->point[i];
+      fscanf(file, "%lf %lf %d", &ppt->c[0], &ppt->c[1], &ppt->ref);
+    }
+  } else if (mesh->dim == 3) {
+    mesh->dim = 2;
+    for (i = 0 ; i < mesh->np ; i++) {
+      ppt = &mesh->point[i];
+      fscanf(file, "%lf %lf %lf %d", &ppt->c[0], &ppt->c[1], &dummy, &ppt->ref); 
+    }
+  }
+
+  /* Edges */
+  rewind(file);
+  fseek(file, posna, SEEK_SET);
+  for (i = 0 ; i < mesh->na ; i++) {
+    pa = &mesh->edge[i];
+    fscanf(file, "%d %d %d", &pa->v[0], &pa->v[1], &pa->ref);
+    pa->v[0]--;
+    pa->v[1]--;
+  }
+
+  /* Triangles */
+  rewind(file);
+  fseek(file, posnt, SEEK_SET);
+  for (i = 0 ; i < mesh->nt ; i++) {
+    pt = &mesh->tria[i];
+    fscanf(file, "%d %d %d %d", &pt->v[0], &pt->v[1], &pt->v[2], &pt->ref);
+    pt->v[0]--;
+    pt->v[1]--;
+    pt->v[2]--;
+
+    pt->flag = 0;
+
+    if (DMG_computeTriaArea(mesh, pt) < 0.) {
+      tmp = pt->v[2];
+      pt->v[2] = pt->v[1];
+      pt->v[1] = tmp;
+    }
+  }
+
   fclose(file);
 
-  return DMG_SUCCESS;
+  return DMG_SUCCESS;  
 }
-
 
 
 int DMG_saveMesh_medit(DMG_pMesh mesh, char *filename) {
