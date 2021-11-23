7f0877d546939d648505d59bd28c383da6de52aa
Gabriel Suau <gabrielsuau2@gmail.com>
2021-11-23 10:41:33 +0100

    Update doc

--- src/dmg.h
+++ src/dmg.h
@@ -30,13 +30,13 @@ void DMG_delPoint(DMG_pMesh mesh, int ip);
 int DMG_newTria(DMG_pMesh mesh);
 void DMG_delTria(DMG_pMesh mesh, int it);
 
-/* Mesh manipulations */
-int DMG_delaunay(DMG_pMesh mesh);         /* Main function */
-int DMG_initDelaunay(DMG_pMesh mesh);     /* Create the 4 vertices of the bounding box and the 2 first triangles */
-int DMG_insertBdryPoints(DMG_pMesh mesh); /* Insert the boundary points in the delaunay triangulation */
-int DMG_forceBndry(DMG_pMesh mesh);
-int DMG_deleteBoundingBox(DMG_pMesh mesh);
-int DMG_insertVolumePoints(DMG_pMesh mesh);
+/* Global mesh manipulations */
+int DMG_delaunay(DMG_pMesh mesh);           /* Main function */
+int DMG_initDelaunay(DMG_pMesh mesh);       /* Create the 4 vertices of the bounding box and the 2 first triangles */
+int DMG_insertBdryPoints(DMG_pMesh mesh);   /* Insert the boundary points in the delaunay triangulation */
+int DMG_forceBndry(DMG_pMesh mesh);         /* Force the domain boundary */
+int DMG_deleteBoundingBox(DMG_pMesh mesh);  /* Delete the triangles of the bounding box */
+int DMG_insertVolumePoints(DMG_pMesh mesh); /* Insert volumes points to achieve a desired edge length */
 
 /* I/O */
 int DMG_loadMesh_medit(DMG_pMesh mesh, char *filename);
